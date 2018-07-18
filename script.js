34de146b5df5e09b4b69307f7313f91de489818f
Joe Moran <joe.moran2@aol.com>
2018-07-18 16:20:15 -0400

    Added Svg images and animations

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     /* Navbar background change*/
     $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
+        let scroll = $(window).scrollTop();
         if (scroll > 150) {
             $(".navbar-default").css("background", "rgb(27, 36, 47)");
         } else {
@@ -24,12 +24,12 @@ $(document).ready(function () {
             // Prevent default anchor click behavior
             event.preventDefault();
             // Store hash
-            var hash = this.hash;
+            let hash = this.hash;
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function () {
+            }, 500, function () {
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
@@ -45,4 +45,104 @@ $(document).ready(function () {
         $(".navbar-fixed-top").css("border-color", "#04ced6")
     });
 
-})
+    //Random Stars
+    const generateStars = () => {
+
+        const $galaxy = $(".galaxy");
+        let iterator = 0;
+        while (iterator <= 70) {
+            const xposition = Math.random();
+            const yposition = Math.random();
+            const star_type = Math.floor((Math.random() * 3) + 1);
+            const position = {
+                "x": $galaxy.width() * xposition,
+                "y": $galaxy.height() * yposition,
+            };
+            $('<div class="star star-type' + star_type + '"></div>').appendTo($galaxy).css({
+                "top": position.y,
+                "left": position.x
+            });
+            iterator++;
+        }
+    };
+
+    generateStars();
+
+    function getDocHeight() {
+        const D = document;
+        return Math.max(
+            D.body.scrollHeight, D.documentElement.scrollHeight,
+            D.body.offsetHeight, D.documentElement.offsetHeight,
+            D.body.clientHeight, D.documentElement.clientHeight
+        )
+    };
+
+    let winheight, docheight, trackLength, throttlescroll;
+
+    function getmeasurements() {
+        winheight = window.innerHeight || (document.documentElement || document.body).clientHeight;
+        docheight = getDocHeight();
+        trackLength = docheight - winheight;
+    };
+
+    function amountscrolled() {
+      const mySvgImg = document.querySelector("#my-photo-svg");
+      const myPhoto = document.querySelector(".my-photo");
+      const mySvgBackground = document.querySelector("#background");
+      const mySvgCupBottom = document.querySelector(".st1");
+      const mySvgCup = document.querySelector(".st2");
+      const mySvgShirt = document.querySelector(".st3");
+      const mySvgHair = document.querySelectorAll(".st4");
+      const mySvgSkin = document.querySelectorAll(".st6");
+      const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+      const scrollTop = window.pageYOffset;
+      const pctScrolled = Math.floor(scrollTop / trackLength * 100);
+        if(width <= 500) {
+            getAmountScolled(18, 26);
+        } else if(width <= 766 && width >= 501) {
+            getAmountScolled(13, 26);
+        } else if(width <= 1066 && width >= 767) {
+            getAmountScolled(32, 50);
+        } else if(width >= 1067) {
+            getAmountScolled(26, 45);
+        }
+        
+        function getAmountScolled(num1, num2) {
+        if(pctScrolled > num1 && pctScrolled < num2) {
+            mySvgImg.classList.add("me-drawing");
+            myPhoto.style.opacity = ".8";
+            mySvgBackground.classList.add("background-color");
+            mySvgCupBottom.classList.add("st1-color");
+            mySvgCup.classList.add("st2-color");
+            mySvgShirt.classList.add("st3-color");
+            mySvgHair.forEach(color => color.classList.add("st4-color"));
+            mySvgSkin.forEach(color => color.classList.add("st6-color"));
+      } else {
+            mySvgImg.classList.remove("me-drawing");
+            myPhoto.style.opacity = "1";
+            mySvgBackground.classList.remove("background-color");
+            mySvgCupBottom.classList.remove("st1-color");
+            mySvgCup.classList.remove("st2-color");
+            mySvgShirt.classList.remove("st3-color");
+            mySvgHair.forEach(color => color.classList.remove("st4-color"));
+            mySvgSkin.forEach(color => color.classList.remove("st6-color"));
+      }
+    }
+
+       
+  };
+
+    getmeasurements();
+
+    window.addEventListener("resize", function () {
+        getmeasurements();
+    }, false);
+
+    window.addEventListener("scroll", function () {
+        clearTimeout(throttlescroll)
+        throttlescroll = setTimeout(function () {
+            amountscrolled();
+        }, 250)
+    }, false);
+    
+});
